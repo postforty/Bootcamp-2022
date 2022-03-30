36676b7fcaa7862b3dee5181b96f4e48b072b2ca
postforty <75767073+postforty@users.noreply.github.com>
2022-03-30 22:27:32 +0900

    [ADD] Vue.js_복습 3차

---
+++ vue/progect02_self_03/src/views/1_databinding/DataBindingInputView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" name="" id="" v-model="userId" />
+    <button @click="myFunction">클릭</button>
+    <button @click="changeData">변경</button>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      userId: 'Sewol'
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    myFunction() {
+      console.log(this.userId)
+    },
+    changeData() {
+      this.userId = 'SEWOL'
+    }
+  }
+}
+</script>
---
+++ vue/progect02_self_03/src/views/HelloView.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hello World</h1>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
+<style scoped></style>
