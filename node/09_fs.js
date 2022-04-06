ddc7fe3e164061466e73a1a6c07e6abd4aa2b27a
postforty <75767073+postforty@users.noreply.github.com>
2022-04-07 00:41:01 +0900

    [ADD] Node.js_수업 1차 복습

--- node/09_fs.js
+++ node/09_fs.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 
-fs.readFile("./sample/text.txt", "utf8", (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log(data);
-});
+// fs.readFile("./sample/text.txt", "utf8", (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(data);
+// });
 
-const data = fs.readFileSync("./sample/text.txt", "utf8");
-console.log(data);
+// const data = fs.readFileSync("./sample/text.txt", "utf8");
+// console.log(data);
 
 const txt = "파일 쓰기 테스트";
 fs.writeFile("./sample/text_w.txt", txt, "utf-8", (err) => {
