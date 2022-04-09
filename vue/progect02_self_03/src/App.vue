040c1106172e861ec70f75f03fc3fbe8390c7629
postforty <75767073+postforty@users.noreply.github.com>
2022-04-10 02:23:26 +0900

    [ADD]Vue.js_복습3차

--- vue/progect02_self_03/src/App.vue
+++ vue/progect02_self_03/src/App.vue
@@ -6,11 +6,13 @@
   </nav> -->
   <HeaderLayout v-if="$route.path != '/'" />
   <router-view />
+  <FooterLayout />
 </template>
 <script>
 import HeaderLayout from '@/components/layouts/HeaderLayout.vue'
+import FooterLayout from '@/components/layouts/FooterLayout.vue'
 export default {
-  components: { HeaderLayout }
+  components: { HeaderLayout, FooterLayout }
 }
 </script>
 <style>
--- vue/progect02_self_03/src/components/layouts/FooterLayout.vue
+++ vue/progect02_self_03/src/components/layouts/FooterLayout.vue
@@ -18,9 +18,8 @@
       <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
         <li class="ms-3">
           <a class="text-muted" href="#"
-            ><svg class="bi" width="24" height="24">
-              <use xlink:href="#twitter"></use></svg
-          ></a>
+            ><i class="fa-brands fa-twitter"></i>
+          </a>
         </li>
         <li class="ms-3">
           <a class="text-muted" href="#"
