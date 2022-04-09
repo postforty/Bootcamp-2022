00148d931bde6d46083068c2b01e72550a1e7ddc
postforty <75767073+postforty@users.noreply.github.com>
2022-04-09 12:23:43 +0900

    [UPDATE] Node.js_수업 복습

--- node/12_route.js
+++ node/12_route.js
@@ -15,15 +15,17 @@ app.get("/ab+cd", (req, res) => {
   res.send("ab+cd");
 });
 
-//
+// b와 C 사이에 아무 것도 없거나 하나 이상의 문자                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 app.get("/ab*cd", (req, res) => {
   res.send("ab*cd");
 });
 
+// a가 포함된 모든 경우
 app.get(/a/, (req, res) => {
   res.send("/a/");
 });
 
+// insert로 시작
 app.get(/^insert/, (req, res) => {
   res.send("/a/");
 });
