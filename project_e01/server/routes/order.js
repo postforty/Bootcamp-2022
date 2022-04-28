9dd0e9f0258047173e90ba66c260fc9d67e09940
postforty <75767073+postforty@users.noreply.github.com>
2022-04-29 00:13:14 +0900

    [UPDATE] javascript_복습1차

--- project_e01/server/routes/order.js
+++ project_e01/server/routes/order.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const mysql = require("../mysql");
 
 router.get("/", async (req, res) => {
-  const customerList = await mysql.query("customerList");
-  res.send(customerList);
+
 });
 
 module.exports = router;
