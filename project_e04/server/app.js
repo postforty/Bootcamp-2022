113f1aee2052074103431fe1a221afec7b1a7c88
postforty <75767073+postforty@users.noreply.github.com>
2022-05-05 23:11:17 +0900

    [update] project_e04_복습

--- project_e04/server/app.js
+++ project_e04/server/app.js
@@ -132,6 +132,74 @@ app.use("/api/product", productRoute);
 //   }
 // });
 
+app.get("/api/file/:filename", (req, res) => {
+  const file = "./uploads/" + req.params.filename; // :filename 정보가 req.params.filename로 들어 옴
+
+  // uploads에 파일이 존재하지 않을 수 있기 때문에 try catch문 사용
+  try {
+    if (fs.existsSync(file)) {
+      // const mimetype = mime.getType(file); // 마임 타입만
+      // const filename = path.basename(file); // 실제 파일 이름만
+      // res.setHeader("Content-disposition", "attachment; filename=" + filename); // 다운 파일명
+      // res.setHeader("Content-type", mimetype); // 파일 형식 지정
+      // const filestream = fs.createReadStream(file); // 버퍼에 담음
+      // filestream.pipe(res); // 다운이 일어남
+      res.download(file); // 위의 코드를 이 한 줄로 해결
+    } else {
+      res.send("요청한 파일이 존재하지 않습니다.");
+    }
+  } catch (e) {
+    console.log(e);
+    res.send("파일을 다운로드 하는 중 에러가 발생했습니다.");
+  }
+});
+
+app.post(
+  "/api/upload/file",
+  fileUpload.single("attachment"),
+  async (req, res) => {
+    console.log(req.file);
+    console.log(req.body);
+
+    const fileInfo = {
+      product_id: parseInt(req.body.product_id),
+      originalname: req.file.originalname,
+      mimetype: req.file.mimetype,
+      filename: req.file.filename,
+      path: req.file.path,
+    };
+
+    res.send(fileInfo);
+
+    // DB에 파일 정보를 저장 관리하고자 할 때
+    // const r = await mysql.query("imageInsert", fileInfo);
+    // res.send(r);
+  }
+);
+
+app.post(
+  "/api/upload/image",
+  fileUpload.single("attachment"),
+  async (req, res) => {
+    console.log(req.file);
+    console.log(req.body);
+
+    const fileInfo = {
+      product_id: parseInt(req.body.product_id),
+      originalname: req.file.originalname,
+      mimetype: req.file.mimetype,
+      filename: req.file.filename,
+      path: req.file.path,
+    };
+
+    res.send(fileInfo);
+
+    // DB에 파일 정보를 저장 관리하고자 할 때
+    // const r = await mysql.query("imageInsert", fileInfo);
+    // res.send(r);
+  }
+);
+
 app.listen(3000, () => {
   console.log("서버가 포트 3000번으로 시작되었습니다.");
 });
