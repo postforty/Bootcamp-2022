112065d6f6e1bd7131048268076642b52616aa94
postforty <75767073+postforty@users.noreply.github.com>
2022-05-07 23:42:50 +0900

    [DELETE] project02

--- vue/project02_참조금지/src/i18n/en.js
+++
@@ -1,8 +0,0 @@
-const i18nEN = {
-  hi: 'hello',
-  search: 'Search',
-  save: 'Save',
-  welcome: 'Hi! Welcome {name}'
-}
-
-export default i18nEN
--- vue/project02_참조금지/src/i18n/ko.js
+++
@@ -1,8 +0,0 @@
-const i18nKO = {
-  hi: '안녕하세요',
-  search: '조회',
-  save: '저장',
-  welcome: '안녕하세요. <br/>{name}님 환영합니다.'
-}
-
-export default i18nKO
