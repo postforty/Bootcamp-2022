36d56c2423cb0018d52cb901e18d37d41776792e
postforty <75767073+postforty@users.noreply.github.com>
2022-05-25 16:20:36 +0900

    [UPDATE] mdb 테스트

--- node_02/test_mdb.js
+++ node_02/test_mdb.js
@@ -1,4 +1,5 @@
 import { readFileSync } from "fs";
+// const fs = require("fs");
 import MDBReader from "mdb-reader";
 
 const buffer = readFileSync("IDCMAINDB.mdb");
@@ -6,6 +7,8 @@ const reader = new MDBReader(buffer);
 
 reader.getTableNames(); // ['Cats', 'Dogs', 'Cars']
 
-const table = reader.getTable("Cats");
+const table = reader.getTable("TWEIGHT");
 table.getColumnNames(); // ['id', 'name', 'color']
 table.getData(); // [{id: 5, name: 'Ashley', color: 'black'}, ...]
+
+console.log(table.getData());
