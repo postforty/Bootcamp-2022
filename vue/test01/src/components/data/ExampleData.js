3a66b42e936d65529ee4aadb3319511bbe412f60
postforty <75767073+postforty@users.noreply.github.com>
2022-06-08 11:34:41 +0900

    [INITIAL] Vue Test용

---
+++ vue/test01/src/components/data/ExampleData.js
@@ -0,0 +1,5 @@
+export default [
+  { title: '서울', code: '02' },
+  { title: '부산', code: '051' },
+  { title: '제주', code: '064' }
+]
---
+++ vue/test01/src/views/TestView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ ExampleData }}</div>
+</template>
+<script>
+import ExampleData from '@/components/data/ExampleData.js'
+export default {
+  components: {},
+  data() {
+    return {
+      ExampleData
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
