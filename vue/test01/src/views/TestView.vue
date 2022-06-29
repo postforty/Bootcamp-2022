95ed75b4eeedf6d26ef92741cab721996db8d3a5
postforty <75767073+postforty@users.noreply.github.com>
2022-06-29 15:12:09 +0900

    [UPDATE] 겸상 서비스 구현 중 테스트

--- vue/test01/src/views/TestView.vue
+++ vue/test01/src/views/TestView.vue
@@ -1,14 +1,44 @@
 <template>
   <div>{{ ExampleData }}</div>
   <button type="" @click="message()">about</button>
+  <button type="" @click="sumScore(mannerQuestions)">sumScore</button>
+  <button type="" @click="checkedButton">checkedButton</button>
+  <button type="" @click="computeScore(mannerQuestions)">computeScore</button>
+  <div :key="manner.id"  v-for="manner in mannerQuestions">
+    <input type="checkbox" name="" id="" :value="manner" v-model="inputTest">{{manner.question}}
+  </div>
+  {{inputTest}}
 </template>
 <script>
+/* eslint-disable */
 import ExampleData from '@/components/data/ExampleData.js'
 export default {
   components: {},
   data() {
     return {
-      ExampleData
+      ExampleData,
+      mannerQuestions: [
+        { id: 'sg1', question: '식사 후 냅킨을 챙겨줘요.', score: 1 },
+        { id: 'sg2', question: '수저를 챙겨줘요.', score: 1 },
+        { id: 'sg3', question: '물을 따라줘요.', score: 1 },
+        { id: 'sg4', question: '부족한 반찬, 음식을 챙겨줘요.', score: 1 },
+        { id: 'sg5', question: '분위기를 편하게 만들어요.', score: 1 },
+        { id: 'sb1', question: '약속 시간을 안 지켜요.', score: -1 },
+        { id: 'sb2', question: '음식을 지저분하게 먹어요.', score: -1 },
+        {
+          id: 'sd3',
+          question: '맛있는 음식을 지나치게 가져가요.',
+          count: 1,
+        },
+        { id: 'sb4', question: '식사 중 대화 예의가 없어요.', score: -1 },
+        { id: 'sb5', question: '돈 정산이 깔끔하지 않아요.', score: -1 },
+        { id: 'bg1', question: '당신은 겸상 매너요정 이에요.', score: 1 },
+        { id: 'bg2', question: '밥장이 밥상을 잘 주도해요.', score: 1 },
+        { id: 'bb1', question: '밥장이 사람을 차별해요.', score: -1 },
+        { id: 'bb2', question: '밥장이 독단적으로 행동해요.', score: -1 },
+      ],
+      ex: [1,2,3,4,5],
+      inputTest: [],
     }
   },
   setup() {},
@@ -18,7 +48,31 @@ export default {
   methods: {
     message() {
       this.$router.push('/about')
+    },
+    sumScore(mq) {
+      const sumall = mq.map(item => item.score).reduce((prev, curr) => prev + curr, 0)
+      console.log(sumall)
+    },
+    checkedButton() {
+      this.inputTest = this.mannerQuestions.slice(0,3)
+    },
+    computeScore(mq) {
+      // const updatedMannerQuestion = mq.map(p => p.id.slice(0,2) === 'sg' ? {...p, score: p.score * 0.02} : p)
+      const updatedMannerQuestion = mq.map(p =>
+				p.id.slice(0, 2) === 'bg'
+					? { ...p, score: p.score * 0.03 }
+					: p.id.slice(0, 2) === 'bb'
+					? { ...p, score: p.score * 0.02 }
+					: p.id.slice(0, 2) === 'sg'
+					? { ...p, score: p.score * 0.02 }
+					: p.id.slice(0, 2) === 'sb'
+					? { ...p, score: p.score * 0.03 }
+					: p,
+			);
+      console.log(updatedMannerQuestion)
     }
+
+
   }
 }
 </script>
